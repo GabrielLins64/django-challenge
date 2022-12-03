197e088f4d45d84579bd9657197b337922eb1046
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-12-02 23:28:50 -0300

    add api login and logout views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import include, path
 from api import views
 
 urlpatterns = [
-    path('', views.api_root),
+    path('', views.api_root, name='API Health check'),
+    path('login/', views.login),
+    path('logout/', views.logout),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from django.contrib.auth import (
+    login as auth_login,
+    logout as auth_logout,
+    authenticate
+)
 from django.http import HttpResponse
+import json
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 
 API_DEFAULT_RESPONSE = "VMS API v1.0"
@@ -10,4 +18,55 @@ API_DEFAULT_RESPONSE = "VMS API v1.0"
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def api_root(request, format=None):
-    return HttpResponse(API_DEFAULT_RESPONSE, content_type="text/plain")
+    """
+    Root API View. It's usable as API Health check,
+    version info and authentication status.
+    """
+    response = API_DEFAULT_RESPONSE
+
+    if request.user.is_authenticated:
+        response += "\nYou're logged in!"
+
+    return HttpResponse(response)
+
+
+@swagger_auto_schema(method='post', request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        'username': openapi.Schema(type=openapi.TYPE_STRING),
+        'password': openapi.Schema(type=openapi.TYPE_STRING),
+    }
+))
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def login(request):
+    """
+    API View for users login. Requires a json body with the
+    properties "username" and "password".
+    """
+    body = request.data
+    username = body.get('username')
+    password = body.get('password')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        auth_login(request, user)
+        return HttpResponse("Successfully logged in!")
+
+    else:
+        return HttpResponse("Invalid credentials", status=401)
+
+
+@api_view(["GET"])
+@permission_classes([permissions.AllowAny])
+def logout(request):
+    """
+    API View for users logout.
+    """
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return HttpResponse("Successfully logged out!")
+    
+    else:
+        return HttpResponse("You are not logged in.")
