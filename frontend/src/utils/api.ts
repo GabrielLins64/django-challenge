553dc50dfed0a5595bead6b756e80d8c1829cdea
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-12-13 01:23:33 -0300

    fetching vulnerabilities by id descending

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -6,7 +6,7 @@ import { retrieveLocalToken } from "./auth";
 
 export const fetchVulnerabilitiesList = async (page: number = 1): Promise<ListResponse> => {
   const apiBaseUrl = process.env.REACT_APP_API_URL;
-  let logoutUrl = apiBaseUrl + `vulnerabilities?page=${page}`;
+  let logoutUrl = apiBaseUrl + `vulnerabilities?order_by=id,desc&page=${page}`;
   let token = retrieveLocalToken();
 
   return axios
